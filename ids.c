48eafb5fd5872a3f7f771c76e52bd1c6347a186b
Michael Pratt <michael@pratt.im>
2012-08-24 16:30:07 -0400

    Camera class opens and closes camera

--- ids.c
+++ ids.c
@@ -1,18 +1,102 @@
 #include <Python.h>
+#include "structmember.h"
 #include <ueye.h>
-
-static PyObject *ids_number_cameras(PyObject *self, PyObject *args);
-static PyObject *ids_camera_list(PyObject *self, PyObject *args);
 
 typedef struct {
     PyObject_HEAD;
+    HIDS handle;
     /* Type fields here */
-} ids_CameraObject;
+} ids_Camera;
+
+static PyMemberDef ids_Camera_members[] = {
+    {"handle", T_UINT, offsetof(ids_Camera, handle), 0, "camera handle"},
+    {NULL}
+};
+
+static PyObject *ids_number_cameras(PyObject *self, PyObject *args) {
+    UEYE_CAMERA_LIST    cameras;
+
+    is_GetNumberOfCameras((int*) &cameras.dwCount);
+
+    return Py_BuildValue("i", cameras.dwCount);
+}
+
+static PyObject *ids_camera_list(PyObject *self, PyObject *args) {
+    UEYE_CAMERA_LIST    cameras;
+
+    is_GetNumberOfCameras((int *) &cameras.dwCount);
+    is_GetCameraList(&cameras);
+
+    PyObject *dict = PyDict_New();
+    PyObject *list = PyList_New(0);
+
+    PyDict_SetItemString(dict, "dwCount", PyInt_FromLong(cameras.dwCount));
+    PyDict_SetItemString(dict, "uci", list);
+
+    for (int i = 0; i < cameras.dwCount; i++) {
+        PyObject *camera_info = PyDict_New();
+
+        PyDict_SetItemString(camera_info, "dwCameraId", Py_BuildValue("I", cameras.uci[i].dwCameraID));
+        PyDict_SetItemString(camera_info, "dwDeviceId", Py_BuildValue("I", cameras.uci[i].dwDeviceID));
+        PyDict_SetItemString(camera_info, "dwSensorId", Py_BuildValue("I", cameras.uci[i].dwSensorID));
+        PyDict_SetItemString(camera_info, "dwInUse", Py_BuildValue("I", cameras.uci[i].dwInUse));
+        PyDict_SetItemString(camera_info, "SerNo", Py_BuildValue("s", cameras.uci[i].SerNo));
+        PyDict_SetItemString(camera_info, "Model", Py_BuildValue("s", cameras.uci[i].Model));
+        PyDict_SetItemString(camera_info, "dwStatus", Py_BuildValue("I", cameras.uci[i].dwStatus));
+
+        PyList_Append(list, camera_info);
+    }
+
+    return dict;
+}
+
+static PyObject *ids_Camera_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+    ids_Camera *self;
+
+    self = (ids_Camera *) type->tp_alloc(type, 0);
+
+    if (self != NULL) {
+        self->handle = -1;
+    }
+
+    return (PyObject *) self;
+}
+
+static int ids_Camera_init(ids_Camera *self, PyObject *args, PyObject *kwds) {
+    static char *kwlist[] = {"handle", NULL};
+
+    self->handle = 0;
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "|i", kwlist, &self->handle)) {
+        return -1;
+    }
+
+    /* TODO: Add (more) error checking */
+    if (is_InitCamera(&self->handle, NULL) != IS_SUCCESS) {
+        return -1;
+    }
+
+    return 0;
+}
+
+static PyObject *ids_Camera_close(ids_Camera *self, PyObject *args, PyObject *kwds) {
+    if (is_ExitCamera(self->handle) != IS_SUCCESS) {
+        Py_INCREF(Py_False);
+        return Py_False;
+    }
+
+    Py_INCREF(Py_True);
+    return Py_True;
+}
+
+static PyMethodDef ids_Camera_methods[] = {
+    {"close", (PyCFunction) ids_Camera_close, METH_VARARGS, "Closes open camera"},
+    {NULL}
+};
 
 static PyTypeObject ids_CameraType = {
     PyVarObject_HEAD_INIT(NULL, 0)
     "ids.Camera",              /* tp_name */
-    sizeof(ids_CameraObject),  /* tp_basicsize */
+    sizeof(ids_Camera),        /* tp_basicsize */
     0,                         /* tp_itemsize */
     0,                         /* tp_dealloc */
     0,                         /* tp_print */
@@ -31,6 +115,23 @@ static PyTypeObject ids_CameraType = {
     0,                         /* tp_as_buffer */
     Py_TPFLAGS_DEFAULT,        /* tp_flags */
     "IDS Camera Object",       /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    ids_Camera_methods,        /* tp_methods */
+    ids_Camera_members,        /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)ids_Camera_init, /* tp_init */
+    0,                         /* tp_alloc */
+    ids_Camera_new,            /* tp_new */
 };
 
 static PyMethodDef  idsMethods[] = {
@@ -64,41 +165,4 @@ int main(int argc, char *argv[]) {
     initids();
 
     return 0;
-}
-
-static PyObject *ids_number_cameras(PyObject *self, PyObject *args) {
-    UEYE_CAMERA_LIST    cameras;
-
-    is_GetNumberOfCameras((int*) &cameras.dwCount);
-
-    return Py_BuildValue("i", cameras.dwCount);
-}
-
-static PyObject *ids_camera_list(PyObject *self, PyObject *args) {
-    UEYE_CAMERA_LIST    cameras;
-
-    is_GetNumberOfCameras(&cameras.dwCount);
-    is_GetCameraList(&cameras);
-
-    PyObject *dict = PyDict_New();
-    PyObject *list = PyList_New(0);
-
-    PyDict_SetItemString(dict, "dwCount", PyInt_FromLong(cameras.dwCount));
-    PyDict_SetItemString(dict, "uci", list);
-
-    for (int i = 0; i < cameras.dwCount; i++) {
-        PyObject *camera_info = PyDict_New();
-
-        PyDict_SetItemString(camera_info, "dwCameraId", Py_BuildValue("I", cameras.uci[i].dwCameraID));
-        PyDict_SetItemString(camera_info, "dwDeviceId", Py_BuildValue("I", cameras.uci[i].dwDeviceID));
-        PyDict_SetItemString(camera_info, "dwSensorId", Py_BuildValue("I", cameras.uci[i].dwSensorID));
-        PyDict_SetItemString(camera_info, "dwInUse", Py_BuildValue("I", cameras.uci[i].dwInUse));
-        PyDict_SetItemString(camera_info, "SerNo", Py_BuildValue("s", cameras.uci[i].SerNo));
-        PyDict_SetItemString(camera_info, "Model", Py_BuildValue("s", cameras.uci[i].Model));
-        PyDict_SetItemString(camera_info, "dwStatus", Py_BuildValue("I", cameras.uci[i].dwStatus));
-
-        PyList_Append(list, camera_info);
-    }
-
-    return dict;
 }
